b5e17df7cd30a2de310d3cf8acf4e1f63cc4180f
Parvizsho Aminov <paminov@cisco.com>
2018-11-04 01:28:57 -0800

    fixing exceptions that are thrown when invalid data is passed, since response is being returned twice

--- hw2/twitRestAPI/api/caller/apicaller.js
+++ hw2/twitRestAPI/api/caller/apicaller.js
@@ -11,7 +11,7 @@ var fs = require('fs');
 //Seraches first 25 entries for the given term
 exports.searchedData = function(req, res) {
 	const un= req.params.username;
-	var params = { 
+	var params = {
 		q: un,
 		count: 25
 	}
@@ -20,7 +20,8 @@ exports.searchedData = function(req, res) {
 		console.log('data:1' + data + 'err:' + err);
 		if (err)
 			res.send(err);
-		res.json(data);
+        else
+		    res.json(data);
 
 	});
 
@@ -28,25 +29,24 @@ exports.searchedData = function(req, res) {
 
 //Tweets the message in the tweeter
 exports.tweeted = function(req, res) {
-	var tweet = { 
-		status: req.body.message }
+	var tweet = { status: req.body.message }
 
-		T.post('statuses/update', tweet, function(err, data, response){
-			console.log('err:' + err);
+    T.post('statuses/update', tweet, function(err, data, response){
+        console.log('err:' + err);
 
-			if (err)
-				res.send(err);			
-			res.json({ reply: 'successfully tweeted' });
-		});
-		
-	};
+        if (err)
+            res.send(err);
+        else
+            res.json({ reply: 'successfully tweeted' });
+    });
+};
 
 //Lists the follower IDs for the given screen name
 exports.followersData = function(req, res) {
 	const un= req.params.screenname;
-	var params = { 
+	var params = {
 		screen_name: un,
-		count: 20 
+		count: 20
 	}
 	console.log('params'+params);
 
@@ -56,54 +56,52 @@ exports.followersData = function(req, res) {
 
 		if (err)
 			res.send(err);
-		res.json(data);
-
+        else
+		    res.json(data);
 	});
-
 };
 
-//retweet a tweet by specifying the tweeter id 
+//retweet a tweet by specifying the tweeter id
 
 exports.retweet = function(req, res) {
-	console.log(req.body);	
-	
-	var retweet = { 
-		id: req.body.id }
+	console.log(req.body);
+
+	var retweet = { id: req.body.id }
 
 	T.post('statuses/retweet/:id', retweet, function(err, data, response){
-			console.log('err:' + err);
+        console.log('err:' + err);
 
-			if (err)
-				res.send(err);			
-			res.json({ reply: 'successfully retwitted' });
-		});
-		
+        if (err)
+            res.send(err);
+        else
+            res.json({ reply: 'successfully retwitted' });
+    });
 };
 
-//Destroy a tweet by specifying the tweeter id 
+//Destroy a tweet by specifying the tweeter id
 
 exports.destroyTweet = function(req, res) {
-	console.log(req.body);	
-	
-	var retweet = { 
-		'id': req.body.id }
+	console.log(req.body);
+
+	var retweet = { 'id': req.body.id }
 
 	T.post('statuses/destroy/:id', retweet, function(err, data, response){
-			console.log('err:' + err);
+        console.log('err:' + err);
 
-			if (err)
-				res.send(err);			
-			res.json({ reply: 'successfully destroyed' });
-		});
-		
+        if (err)
+            res.send(err);
+        else
+            res.json({ reply: 'successfully destroyed' });
+    });
+
 };
 
 //Slug data display
 exports.slugData = function(req, res) {
 	const un= req.params.slugCategory;
-	var params = { 
+	var params = {
 		slug: un,
-		count: 20 
+		count: 20
 	}
 	console.log('params'+params);
 
@@ -113,47 +111,45 @@ exports.slugData = function(req, res) {
 
 		if (err)
 			res.send(err);
-		res.json(data);
-
+        else
+		    res.json(data);
 	});
-
 };
 
 
 //Post a media
 exports.mediaPost = function(req, res) {
 
-var b64content = fs.readFileSync('cloud-storage-services-848x480.jpg', { encoding: 'base64' })
- 
-// first we must post the media to Twitter
-T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-  // now we can assign alt text to the media, for use by screen readers and
-  // other text-based presentations and interpreters
-  var mediaIdStr = data.media_id_string;
-  var altText = req.body.altText;
-  var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
- 
-  T.post('media/metadata/create', meta_params, function (err, data, response) {
-    if (!err) {
-      // now we can reference the media and post a tweet (media will attach to the tweet)
-      var params = { status: req.body.message, media_ids: [mediaIdStr] }
- 
-      T.post('statuses/update', params, function (err, data, response) {
-       if (err)
-				res.send(err);			
-			res.json({ reply: 'successfully posted image' });
-      });
-    };
-  });
-});
+    var b64content = fs.readFileSync('cloud-storage-services-848x480.jpg', { encoding: 'base64' })
+
+    // first we must post the media to Twitter
+    T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+        // now we can assign alt text to the media, for use by screen readers and
+        // other text-based presentations and interpreters
+        var mediaIdStr = data.media_id_string;
+        var altText = req.body.altText;
+        var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
+
+        T.post('media/metadata/create', meta_params, function (err, data, response) {
+            if (!err) {
+                // now we can reference the media and post a tweet (media will attach to the tweet)
+                var params = { status: req.body.message, media_ids: [mediaIdStr] }
+
+                T.post('statuses/update', params, function (err, data, response) {
+                    if (err)
+                        res.send(err);
+                    else
+                        res.json({ reply: 'successfully posted image' });
+                });
+            };
+        });
+    });
 };
-	
+
 //Lists the friends IDs for the given screen name-returns user id of teh following entities.
 exports.friendsData = function(req, res) {
 	const un= req.params.userId;
-	var params = { 
-		user_id: un		 
-	}
+	var params = { user_id: un }
 	console.log('params'+params);
 
 	T.get('friends/ids', params,function(err, data, response){
@@ -162,12 +158,12 @@ exports.friendsData = function(req, res) {
 
 		if (err)
 			res.send(err);
-		res.json(data);
+        else
+		    res.json(data);
 
 	});
-
 };
-	
-	
+
+
 
 
